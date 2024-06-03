70fa119070e7db9612b6b2bdbafd7b56731da83f
Sricor <node@ioaths.com>
2024-06-04 01:17:10 +0800

    test: complete transaction rate costs test (#13)

--- src/trade/mod.rs
+++ src/trade/mod.rs
@@ -117,3 +117,35 @@ pub enum TradeSide {
     #[serde(rename = "SELL")]
     Sell,
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::types::Decimal;
+
+    use super::Trade;
+
+    fn dec(value: &str) -> Decimal {
+        use std::str::FromStr;
+        Decimal::from_str(value).unwrap()
+    }
+    
+    #[tokio::test]
+    async fn test_costs() {
+        let trade = Trade::with_buy(dec("10"), dec("5"), dec("50"));
+        assert_eq!(trade.costs(), dec("0"));
+
+        let trade = Trade::with_buy(dec("50"), dec("0.3996"), dec("20.0"));
+        assert_eq!(trade.costs(), dec("0.02"));
+
+        let trade = Trade::with_buy(dec("507.545135202621"), dec("0.09841489"), dec("50"));
+        assert_eq!(trade.costs(), dec("0.0500013489989265733099999825"));
+
+        let trade = Trade::with_sell(dec("509.067770608228"), dec("0.098"), dec("49.8387528781"));
+        assert_eq!(trade.costs(), dec("0.049888641506344"));
+
+        let trade = Trade::with_sell(dec("200"), dec("0.3996"), dec("79.84008"));
+        assert_eq!(trade.costs(), dec("0.07992"));
+    }
+}
