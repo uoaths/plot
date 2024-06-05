2976c938af64353f6f941373236f3c766dafc3ce
Sricor <node@ioaths.com>
2024-06-05 16:43:56 +0800

    feat: grid percent stop lost support (#14)
    * feat: grid percent stop lost support
    
    * chore: fmt

--- src/strategy/grid_percent.rs
+++ src/strategy/grid_percent.rs
@@ -10,14 +10,21 @@ pub struct GridPercent {
     pub investment: QuoteQuantity,
     pub range: Range<Price>,
     pub percent: Decimal,
+    pub percent_lost: Decimal,
 }
 
 impl GridPercent {
-    pub fn new(investment: QuoteQuantity, range: Range<Price>, percent: Decimal) -> Self {
+    pub fn new(
+        investment: QuoteQuantity,
+        range: Range<Price>,
+        percent: Decimal,
+        percent_lost: Decimal,
+    ) -> Self {
         Self {
             investment,
             range,
             percent,
+            percent_lost,
         }
     }
 }
@@ -27,6 +34,7 @@ impl Strategy for GridPercent {
         let initial_price = self.range.min().clone();
         let termination_price = self.range.max().clone();
         let percentage_increase = Decimal::ONE + self.percent;
+        let percentage_lost = Decimal::ONE - self.percent_lost;
 
         let mut prices = vec![initial_price];
         loop {
@@ -63,9 +71,20 @@ impl Strategy for GridPercent {
                 return positions;
             }
 
+            let selling_prices = {
+                if Decimal::ZERO < percentage_lost && percentage_lost < Decimal::ONE {
+                    vec![
+                        Range(sell_0, termination_price.clone()),
+                        Range(Decimal::ZERO, sell_0 * percentage_lost),
+                    ]
+                } else {
+                    vec![Range(sell_0, termination_price.clone())]
+                }
+            };
+
             positions.push(Position {
                 buying_prices: vec![Range(buy_0, buy_1)],
-                selling_prices: vec![Range(sell_0, termination_price.clone())],
+                selling_prices: selling_prices,
                 base_quantity: Decimal::ZERO,
                 quote_quantity: self.investment.clone(),
             });
@@ -88,7 +107,12 @@ mod tests {
 
     #[test]
     fn test_positions() {
-        let grid = GridPercent::new(dec("100"), Range(dec("50"), dec("60")), dec("0.01"));
+        let grid = GridPercent::new(
+            dec("100"),
+            Range(dec("50"), dec("60")),
+            dec("0.01"),
+            dec("0"),
+        );
         let positions = grid.assign_position();
 
         assert_eq!(
@@ -121,7 +145,12 @@ mod tests {
             ]
         );
 
-        let grid = GridPercent::new(dec("100"), Range(dec("100"), dec("200")), dec("0.05"));
+        let grid = GridPercent::new(
+            dec("100"),
+            Range(dec("100"), dec("200")),
+            dec("0.05"),
+            dec("0"),
+        );
         let positions = grid.assign_position();
 
         assert_eq!(
@@ -142,6 +171,50 @@ mod tests {
                 Position {
                     buying_prices: vec![Range(dec("147.745544378906"), dec("155.132821597851"))],
                     selling_prices: vec![Range(dec("162.889462677743"), dec("200"))],
+                    base_quantity: dec("0"),
+                    quote_quantity: dec("100")
+                }
+            ]
+        );
+    }
+
+    #[test]
+    fn test_positions_stop_lost() {
+        let grid = GridPercent::new(
+            dec("100"),
+            Range(dec("100"), dec("200")),
+            dec("0.05"),
+            dec("0.1"),
+        );
+        let positions = grid.assign_position();
+
+        assert_eq!(
+            positions,
+            vec![
+                Position {
+                    buying_prices: vec![Range(dec("100"), dec("105"))],
+                    selling_prices: vec![
+                        Range(dec("110.25"), dec("200")),
+                        Range(dec("0"), dec("99.225"))
+                    ],
+                    base_quantity: dec("0"),
+                    quote_quantity: dec("100")
+                },
+                Position {
+                    buying_prices: vec![Range(dec("121.550625"), dec("127.62815625"))],
+                    selling_prices: vec![
+                        Range(dec("134.0095640625"), dec("200")),
+                        Range(dec("0"), dec("120.60860765625"))
+                    ],
+                    base_quantity: dec("0"),
+                    quote_quantity: dec("100")
+                },
+                Position {
+                    buying_prices: vec![Range(dec("147.745544378906"), dec("155.132821597851"))],
+                    selling_prices: vec![
+                        Range(dec("162.889462677743"), dec("200")),
+                        Range(dec("0"), dec("146.6005164099687"))
+                    ],
                     base_quantity: dec("0"),
                     quote_quantity: dec("100")
                 }
